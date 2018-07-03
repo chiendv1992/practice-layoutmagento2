08e8b30096c7c4b9cba266ad2a481fa7e516140e
chiendv <chiendv1992@gmail.com>
2018-07-04 01:17:52 +0700

    example get sku product

---
+++ app/code/Example/Object/Block/ProductRepositoryInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Example\Object\Block;
+class ProductRepositoryInterface extends \Magento\Framework\View\Element\Template
+{
+    protected $_productRepository;
+
+    public function __construct(
+        \Magento\Backend\Block\Template\Context $context,
+        \Magento\Catalog\Model\ProductRepository $productRepository,
+        array $data = []
+    )
+    {
+        $this->_productRepository = $productRepository;
+        parent::__construct($context, $data);
+    }
+
+    public function getProductById($id)
+    {
+        return $this->_productRepository->getById($id);
+    }
+
+    public function getProductBySku($sku)
+    {
+        return $this->_productRepository->get($sku);
+    }
+}
+?>
